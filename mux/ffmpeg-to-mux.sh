efa6324bf6d0a862685fb56c59e47e20078e9eaa
x71c9 <108585118+x71c9@users.noreply.github.com>
2025-04-19 11:43:36 +0200

    feat: minimal setup nginx. better mux index.html.

--- mux/ffmpeg-to-mux.sh
+++ mux/ffmpeg-to-mux.sh
@@ -3,17 +3,21 @@
 # Explanation:
 #     -i is the input option.
 #     "0:none" refers to the input device index for the avfoundation capture system (used on macOS).
-# 
+#
 # In "0:none":
 #     0 is the index of the video device (camera).
 #     none means no audio input.
-# 
+#
 # So:
 #     "0:none" uses the first available camera (index 0).
 #     "1:none" would use the second camera, and so on.
 
-# Replace this with your actual Mux stream key
-MUX_STREAM_KEY="1b790b93-85ab-1071-7bd9-66615cd54fe2"
+# Get the Mux stream key from the first argument or prompt the user
+if [ -z "$1" ]; then
+  read -p "Enter your Mux stream key: " MUX_STREAM_KEY
+else
+  MUX_STREAM_KEY="$1"
+fi
 
 MUX_URL="rtmps://global-live.mux.com:443/app"
 
--- nginx/ffmpeg-to-nginx.sh.sh
+++ nginx/ffmpeg-to-nginx.sh.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Stream directly to the hls application on NGINX
-# RTMP_URL="rtmp://localhost:1935/hls/live"
-RTMP_URL="rtmp://localhost:1935/stream/live"
+RTMP_URL="rtmp://localhost:1935/hls/live"
 
 ffmpeg \
   -f avfoundation -framerate 30 -video_size 1280x720 -pixel_format uyvy422 -i "0:none" \

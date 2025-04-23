4fa2f0d6457d3a9d6f2728f1f921d6ea914780bb
x71c9 <108585118+x71c9@users.noreply.github.com>
2025-04-23 11:15:54 +0200

    feat: autoplay stream html, stop when not visible, no controls. index.m3u8 no cache.

--- aws/camerahost/scripts/upload-hls-to-s3.sh
+++ aws/camerahost/scripts/upload-hls-to-s3.sh
@@ -15,5 +15,15 @@ inotifywait -m -r -e close_write,create,delete "$SCRIPT_DIR/../hls" --format '%w
     --region $REGION \
     --delete \
     --exact-timestamps
+
+  # Force no-cache headers for index.m3u8
+  if [[ -f "$SCRIPT_DIR/../hls/index.m3u8" ]]; then
+    echo "Uploading index.m3u8 with no-cache headers"
+    /usr/local/bin/aws s3 cp "$SCRIPT_DIR/../hls/index.m3u8" "s3://$BUCKET_NAME/stream/index.m3u8" \
+      --region "$REGION" \
+      --cache-control "no-cache, no-store, must-revalidate" \
+      --content-type "application/vnd.apple.mpegurl"
+  fi
+
 done
 

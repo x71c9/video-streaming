1faadb282888c50b1d4fbc3b62e10a7e9c772bce
x71c9 <108585118+x71c9@users.noreply.github.com>
2025-04-25 17:38:07 +0200

    feat: print generate .env in terraform apply

--- gcp/terraform-apply.sh
+++ gcp/terraform-apply.sh
@@ -10,3 +10,27 @@ if [ "$ASSUME_YES" = true ]; then
 else
   terraform apply -var-file="config/environment.tfvars"
 fi
+
+echo "------------------------------------------------------------------------"
+echo
+echo "Now you can generate a .env file needed for the scripts in ./camerahost"
+echo "by running:"
+echo
+echo -e "\033[1m  bash generate-dotenv.sh\033[0m"
+echo
+echo "The .env file will include:"
+echo "  - BUCKET_NAME"
+echo "  - REGION"
+echo "  - ALERT_EMAIL"
+echo "  - MAILGUN_API_KEY"
+echo "  - MAILGUN_DOMAIN"
+echo
+echo "Alternatively, you can get them manually with:"
+echo
+echo "  BUCKET_NAME=\$(terraform output -raw stream_bucket_name)"
+echo "  REGION=\$(terraform output -raw region)"
+echo "  ALERT_EMAIL=\$(terraform output -raw alert_email)"
+echo "  MAILGUN_API_KEY="
+echo "  MAILGUN_DOMAIN="
+echo
+
